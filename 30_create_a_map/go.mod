b72a796877bb9280ab34d70c386aa80b5a26859a
Ilyas Ouzrour <ilyas.ouzrour@gmail.com>
2025-03-11 22:54:03 +0000

    Create a map and print key-value pairs

---
+++ 30_create_a_map/go.mod
@@ -0,0 +1,3 @@
+module github.com/ouzrourextra/go_medium_lvl/30_create_a_map
+
+go 1.23.5
---
+++ 30_create_a_map/main.go
@@ -0,0 +1,71 @@
+package main
+
+
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"strconv"
+	"strings"
+	"os"
+)
+
+/*
+inputNumber is a function for inputing a number
+*/
+func inputNumber(sentence string) (nb int64, err error) {
+	fmt.Println("=======================")
+	fmt.Printf("%s: ",sentence)
+	reader := bufio.NewReader(os.Stdin)
+	str, _ := reader.ReadString('\n')
+	str = strings.TrimSpace(str)
+	nb, err1 := strconv.ParseInt(str, 10, 64)
+	if err1 != nil {
+		return 0, err1
+	} else {
+		return nb, nil
+	}
+}
+
+func fillTheMap(MyMap *map[string]int,capacity int){
+	fmt.Printf("\n=========================")
+
+	for i:= 0 ; i<capacity ; i++ {
+		fmt.Printf("\nIndex of Element %d (string) : ",i+1)
+		readerString := bufio.NewReader(os.Stdin)
+		String , errString := readerString.ReadString('\n')
+		if errString != nil {
+			log.Fatal(errString)
+		}
+		fmt.Printf("Value of Element %d (int) : ",i+1)
+
+		readerInt := bufio.NewReader(os.Stdin)
+		IntString , errInt := readerInt.ReadString('\n')
+		if errInt != nil{
+			log.Fatal(errInt)
+		}
+		Int , errConv := strconv.Atoi(strings.TrimSpace(IntString))
+		if errConv != nil{
+			log.Fatal(errConv)
+		}
+		(*MyMap)[String] = Int
+		fmt.Printf("INFO : Added Successfully !\n")
+		fmt.Printf("=========================\n")
+
+	}
+}
+
+func main(){
+	numberOfElements , err := inputNumber("Enter the number of Elements in the map")
+	if err != nil{
+		log.Fatal(err)
+	}
+	mapCreated := make(map[string]int)
+
+	fillTheMap(&mapCreated,int(numberOfElements))
+
+	for index , value := range mapCreated{
+		fmt.Printf("map['%s']=%d\n",index,value)
+	}
+}
